59603f84ca9bd27ba0373ae784a122d61c9a4f82
Jean GAHOUSSOU <gahoussoujean6475@gmail.com>
2025-08-06 18:44:05 -0400

    Portofolio 17e commit, Fin de Javascript

--- JS/global.js
+++ JS/global.js
@@ -64,14 +64,75 @@ tabsFilters()
 
 function showProjectDetails() {
   const links = document.querySelectorAll('.card__link');
-  const modal = document.querySelector('.modal');
+  const modals = document.querySelectorAll('.modal');
+  const closeButtons = document.querySelectorAll('.modal__close');
+
+  const hideModals = () => {
+    modals.forEach(modal => {
+      modal.classList.remove('show');
+    });
+  }
 
   links.forEach(elem => {
     elem.addEventListener('click', (event) => {
       event.preventDefault();
-      document.querySelector(`[id="${elem.dataset.id}]`).classList.add('show');
+      
+      document.querySelector(`[id="${elem.dataset.id}"]`).classList.add('show');
+    });
+  });
+
+  closeButtons.forEach(button => {
+    button.addEventListener('click', (event) => {
+      hideModals();
     });
   });
 }
 
 showProjectDetails()
+
+// effets
+
+const observerIntersectionAnimation = () => {
+  const sections = document.querySelectorAll('section');
+  const skills = document.querySelectorAll('.Skills .bar');
+
+  sections.forEach((section, index) => {
+    if (index === 0) return;
+    section.style.opacity = "0";
+    section.style.transition = "all 1.6s";
+  });
+
+  skills.forEach((elem, index) => {
+
+    elem.style.width = "0";
+    elem.style.transition = "all 1.6s";
+  });
+
+  let sectionObserver = new IntersectionObserver(function (entries, observer) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        let elem = entry.target;
+        elem.style.opacity = 1;
+      }
+    });
+  });
+
+  sections.forEach(section => {
+    sectionObserver.observe(section);
+  });
+
+  let skillsObserver = new IntersectionObserver(function (entries, observer) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        let elem = entry.target;
+        elem.style.width = elem.dataset.width + '%';
+      }
+    });
+  });
+
+  skills.forEach(skill => {
+    skillsObserver.observe(skill);
+  });
+}
+
+observerIntersectionAnimation();
