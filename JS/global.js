31d6c100ab285cd635ad5e1d5b399ee7817b579d
Jean GAHOUSSOU <gahoussoujean6475@gmail.com>
2025-08-06 16:54:47 -0400

    Portofolio 16e commit, Portofolio Javascripts

--- JS/global.js
+++ JS/global.js
@@ -16,3 +16,62 @@ function menuMobile() {
 }
 
 menuMobile();
+
+// Portofolio
+
+function tabsFilters() {
+  const tabs = document.querySelectorAll('.Portofolio-filter a');
+  const projets = document.querySelectorAll('.Portofolio .card');
+
+  const resetActiveLinks = () => {
+    tabs.forEach(elem => {
+      elem.classList.remove('active');
+    })
+  }
+
+  const showProjets = (elem) => {
+    console.log(elem)
+    projets.forEach(projet => {
+      let filter = projet.getAttribute('data-category');
+
+      if (elem === 'all') {
+        projet.parentNode.classList.remove('hide');
+        return
+      }
+
+      if (filter !== elem) {
+        projet.parentNode.classList.add('hide');
+      } else {
+        projet.parentNode.classList.remove('hide');
+      }
+    });
+  }
+
+  tabs.forEach(elem => {
+    elem.addEventListener('click', (event) => {
+      event.preventDefault();
+      let filter = elem.getAttribute("data-filter");
+      showProjets(filter)
+      resetActiveLinks();
+      elem.classList.add('active');
+    });
+  })
+
+
+}
+
+tabsFilters()
+
+function showProjectDetails() {
+  const links = document.querySelectorAll('.card__link');
+  const modal = document.querySelector('.modal');
+
+  links.forEach(elem => {
+    elem.addEventListener('click', (event) => {
+      event.preventDefault();
+      document.querySelector(`[id="${elem.dataset.id}]`).classList.add('show');
+    });
+  });
+}
+
+showProjectDetails()
